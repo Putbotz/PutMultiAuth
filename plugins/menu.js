84982fe8f17bc9d71c6ded5b1cd4903c755cbf4d
Putbotz <96361236+Putbotz@users.noreply.github.com>
2023-07-19 23:55:29 +0800

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -160,7 +160,7 @@ text = typeof conn.menu == 'string' ? conn.menu : typeof conn.menu == 'object' ?
       level, limit, name, weton, week, date, dateIslamic, wib, wit, wita, time, totalreg, rtotalreg, role
     }
     text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
-  conn.sendFile(m.chat, `https://telegra.ph/file/dc5a67d724b016574129b.jpg`, null, text, m)
+  conn.sendFile(m.chat, `https://telegra.ph/file/0332ac3e7db05ec972ee4.jpg`, null, text, m)
   } catch (e) {
     conn.reply(m.chat, 'Maaf, menu sedang error', m)
     throw e
