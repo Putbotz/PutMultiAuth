be93e48c8683b2183e601d81dd72152dc6b8614f
Putbotz <96361236+Putbotz@users.noreply.github.com>
2023-08-03 14:43:51 +0800

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -48,7 +48,7 @@ let tags = {
 const defaultMenu = {
   before: `
 Hi %name
-I am an automated system (Putbotz) that can help to do something, search and get data / information only through WhatsApp.
+I am an automated system (Admin Misi Puchong) that can help to do something, search and get data / information only through WhatsApp.
 
  ◦  *Library:* Baileys
  ◦  *Function:* Assistant
@@ -160,7 +160,7 @@ text = typeof conn.menu == 'string' ? conn.menu : typeof conn.menu == 'object' ?
       level, limit, name, weton, week, date, dateIslamic, wib, wit, wita, time, totalreg, rtotalreg, role
     }
     text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
-  conn.sendFile(m.chat, `https://telegra.ph/file/0332ac3e7db05ec972ee4.jpg`, null, text, m)
+  conn.sendFile(m.chat, `https://telegra.ph/file/8c802b446887e0472252a.jpg`, null, text, m)
   } catch (e) {
     conn.reply(m.chat, 'Maaf, menu sedang error', m)
     throw e
