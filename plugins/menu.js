1bfff1783eb6b27eb4fbf453be7f180d6284ceb7
Putbotz <96361236+Putbotz@users.noreply.github.com>
2024-01-09 20:26:59 +0800

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -48,7 +48,7 @@ let tags = {
 const defaultMenu = {
   before: `
 Hi %name
-I am an automated system (Putbotz) that can help to do something, search and get data / information only through WhatsApp.
+I am an automated system (Gebuu) that can help to do something, search and get data / information only through WhatsApp.
 
  ◦  *Library:* Baileys
  ◦  *Function:* Assistant
