fb746a10708f0f98bc03d58712f25285674fe4ef
Putbotz <96361236+Putbotz@users.noreply.github.com>
2024-03-19 15:05:07 +0800

    Update group-tagall.js

--- plugins/group-tagall.js
+++ plugins/group-tagall.js
@@ -1,12 +1,11 @@
 let handler = async(m, { conn, text, participants }) => {
   let teks = `⋙ *TAGALL* ⋘
-\n *${text ? text : 'By @tagall_wiya 👾
-'}*\n\n`
+\n *${text ? text : 'by 🧾﹢ 𝖻𝗈𝗍 𝗐𝗂𝗒𝖺𝗌𝗍𝗈𝗋𝖾 ྀ   ׅ ࣪ ⁺'}*\n\n`
 		      	for (let mem of participants) {
 		            teks += `│◦❒ @${mem.id.split('@')[0]}\n`
 				}
                 teks += `
-TagAll Members By wa.me/601169466091`
+TagAll Members By 🧾﹢ 𝖻𝗈𝗍 𝗐𝗂𝗒𝖺𝗌𝗍𝗈𝗋𝖾 ྀ   ׅ ࣪ ⁺`
                 conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
 }
 handler.help = ['tagall <pesan>']
