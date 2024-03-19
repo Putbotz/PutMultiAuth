52d09408078eb5d91db0d115699c72526e899b45
Putbotz <96361236+Putbotz@users.noreply.github.com>
2024-03-19 15:02:36 +0800

    Update tes123.js

--- plugins/tes123.js
+++ plugins/tes123.js
@@ -1,6 +1,9 @@
 let handler = async m => {
 
-let intro = `@wiya_botberbayar🤖`
+let intro = `🧾﹢ 𝖻𝗈𝗍 𝗐𝗂𝗒𝖺𝗌𝗍𝗈𝗋𝖾 ྀ   ׅ ࣪ ⁺
+
+: 𝗉𝗆 𝗎𝗇𝗍𝗎𝗄 𝗌𝖾𝗐𝖺 
+https://wa.me/601170054254?text=@𝘄𝗶𝘆𝗮𝘀𝘁𝗼𝗿𝗲`
 m.reply(intro)
 }
 handler.customPrefix = /^(tes|tess|test)$/i
